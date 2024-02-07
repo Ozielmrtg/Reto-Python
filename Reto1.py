f8e41b99a747167bbf32f361041d083f12449ffc
Ozielmrtg <159223508+Ozielmrtg@users.noreply.github.com>
2024-02-07 00:37:47 -0600

    Add files via upload

---
+++ Reto1.py
@@ -0,0 +1,13 @@
+#Ingreso de nuevos usuarios
+
+print ('Teclee a continuación los datos requeridos') #inicio
+
+#ingreso de datos
+nombre = input('Ingrese su nombre: ')
+apellido_paterno = input('Ingrese su apellido paterno: ')
+apellido_materno = input('Ingrese su apellido materno: ')
+telefono = int = input('Ingrese su numero de telefono: ')
+correo_electronico = input('Ingrese una dirección de correo electrónico válido: ')
+
+#bienvenida
+print('Bienvenido ' + nombre + ' ' + apellido_paterno + ' ' + apellido_materno + ', en breve recibirás un correo a ' + correo_electronico + '.')
